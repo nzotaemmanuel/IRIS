a27532d12082a0cadd55798dff83d42bbaecfdcd
Nzota Emmanuel <nzotaemmanuel16@gmail.com>
2026-05-30 17:56:20 +0100

    Fix test data SQL query

--- test_data.ts
+++ test_data.ts
@@ -5,17 +5,17 @@ async function test() {
     try {
         console.log('--- TESTING TOWER/MAST DATA ---');
         const towers = await executeQuery(`
-            SELECT TOP 5 r.ID, r.RequestTitle, r.ApplicationDate, r.LocalGovernmentArea_
+            SELECT TOP 5 r.RequestID, r.RequestTitle, r.ApplicationDate
             FROM [SmartBoxData].[LASIMRA_Request_SMO] r
-            INNER JOIN [SmartBoxData].[LASIMRA_TowerMast_Reqeust_SMO] tm ON r.ID = tm.RequestID
+            INNER JOIN [SmartBoxData].[LASIMRA_TowerMast_Reqeust_SMO] tm ON r.RequestID = tm.RequestID
         `);
         console.log('Tower Result:', JSON.stringify(towers, null, 2));
 
         console.log('\n--- TESTING ROW DATA ---');
         const rows = await executeQuery(`
-            SELECT TOP 5 r.ID, r.RequestTitle, r.ApplicationDate, r.LocalGovernmentArea_
+            SELECT TOP 5 r.RequestID, r.RequestTitle, r.ApplicationDate, rr.LocalGovernmentArea
             FROM [SmartBoxData].[LASIMRA_Request_SMO] r
-            INNER JOIN [SmartBoxData].[LASIMRA_RowRequest_SMO] rr ON r.ID = rr.RequestID
+            INNER JOIN [SmartBoxData].[LASIMRA_RowRequest_SMO] rr ON r.RequestID = rr.RequestID
         `);
         console.log('RoW Result:', JSON.stringify(rows, null, 2));
     } catch (err) {
